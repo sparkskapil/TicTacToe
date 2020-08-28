1b161bae98cd5a265b55d3e1e11ad6df70a8f24b
Kapil Verma <sparkskapil@gmail.com>
2020-08-29 02:20:48 +0530

    Added Utility methods in Scriptable class Updated Script to attach x_o sprites on button click.
    Added Event function to convert method to onclickevent for button compoenent

--- TicTacToeGui/ECS/Scriptable.py
+++ TicTacToeGui/ECS/Scriptable.py
@@ -1,3 +1,8 @@
+from ECS.Components import TagComponent
+
+def Event(func: callable, *args, **kwargs):
+    return lambda: func(*args, **kwargs)
+
 class Scriptable:
     def __init__(self, scene, entity):
         self.scene = scene
@@ -8,6 +13,27 @@ class Scriptable:
     def GetComponent(self, Typename):
         return self.Entity.GetComponent(Typename)
 
+    def GetComponents(self):
+        return self.Entity.GetComponents()
+    
+    def GetSceneComponentsByType(self, typename):
+        return self.Reg.GetComponentsByType(typename)
+    
+    def GetEntity(self, entityId):
+        return self.scene.Entities[entityId]
+    
+    def GetEntitiesByTag(self, tagname):
+        tags = self.GetSceneComponentsByType(TagComponent)
+        entities = list()
+        for tag, enttId in tags:
+            if tag.name == tagname:
+                entities.append(self.scene.Entities[enttId])
+        return entities
+           
+    def AddOnClickListener(self, button, event:callable):
+        self.scene.InputHandler.AddOnClickListener(button, event)
+    
+     
     # Will contains instance of
     # 1. Physics Manager
     # 2. Input Manager
--- TicTacToeGui/Script.py
+++ TicTacToeGui/Script.py
@@ -1,5 +1,5 @@
-from ECS.Scriptable import Scriptable
-from ECS.Components import TransformComponent, ButtonComponent
+from ECS.Scriptable import Scriptable, Event
+from ECS.Components import ButtonComponent, TagComponent, SpriteComponent
 
 
 class GameScript(Scriptable):
@@ -7,15 +7,30 @@ class GameScript(Scriptable):
         Scriptable.__init__(self, scene, entity)
 
     def Setup(self):
-        self.count = 0
-        buttonComponents = self.scene.Reg.GetComponentsByType(ButtonComponent)
+        self.xSprite = SpriteComponent("T3X_BLACK.png")
+        self.xSprite.width = 100
+        self.xSprite.mode = SpriteComponent.SpriteMode.RespectAspect
+
+        self.oSprite = SpriteComponent("T3O_BLACK.png")
+        self.oSprite.width = 100
+        self.oSprite.mode = SpriteComponent.SpriteMode.RespectAspect
+
+        self.turn = 0
+
+        buttonComponents = self.GetSceneComponentsByType(ButtonComponent)
         for component, entt in buttonComponents:
-            component.action = lambda: self.onButtonClick(entt)
+            entity = self.GetEntity(entt)
+            self.AddOnClickListener(
+                component, Event(self.onButtonClick, entity))
 
     def Update(self):
-        if self.count < 1:
-            print("Game Object Updating...")
-        self.count += 1
+        pass
 
     def onButtonClick(self, entity):
-        print("ButtonClicked Entity {}".format(entity))
+        if self.turn == 0:
+            entity.AddComponent(self.xSprite)
+        else:
+            entity.AddComponent(self.oSprite)
+        entity.GetComponent(ButtonComponent).enabled = False
+        self.turn += 1
+        self.turn %= 2

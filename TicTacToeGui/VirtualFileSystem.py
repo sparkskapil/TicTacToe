16f326fd4135f1e30be1e9f01a1169564e2a99f7
Kapil Verma <sparkskapil@gmail.com>
2020-09-12 23:49:22 +0530

    Added methods to import different files

--- TicTacToeGui/VirtualFileSystem.py
+++ TicTacToeGui/VirtualFileSystem.py
@@ -33,6 +33,67 @@ class VirtualFileSystem:
                 self.ForwardDirectories.clear()
             self.RefreshContents()
 
+    def CreateFolder(self, foldername):
+        """
+        Creates a folder with specified name in the current directory
+        """
+        folderToCreate = FileSystem.JoinPath(self.CurrentDirectory, foldername)
+        if FileSystem.IsValidDirectory(folderToCreate) or FileSystem.IsValidFile(folderToCreate):
+            raise Exception("File or Folder with same name already exists.")
+        FileSystem.CreateDirectory(folderToCreate)
+        self.RefreshContents()
+
+    def ImportSprite(self, sprite_path):
+        """
+        Imports image/sprite files to the current directory after validating
+        """
+        supportedFormats = ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')
+        if not sprite_path.lower().endswith(supportedFormats):
+            raise Exception("The selected sprite format is not supported.")
+        self.__importFileToCurrentDirectory(sprite_path)
+
+    def ImportFont(self, font_path):
+        """
+        Imports font files to the current directory after validating
+        """
+        supportedFormats = ('.otf', '.ttf', '.fnt')
+        if not font_path.lower().endswith(supportedFormats):
+            raise Exception("The selected font is not supported.")
+        self.__importFileToCurrentDirectory(font_path)
+
+    def ImportScript(self, script_path):
+        """
+        Imports python script files to the current directory after validating
+        """
+        supportedFormats = ('.py')
+        if not script_path.lower().endswith(supportedFormats):
+            raise Exception("The selected file is not a python file.")
+        self.__importFileToCurrentDirectory(script_path)
+
+    def ImportScene(self, scene_path):
+        """
+        Imports ProtoTypeEngine Scene files to the current directory after validating
+        """
+        supportedFormats = ('.pts')
+        if not scene_path.lower().endswith(supportedFormats):
+            raise Exception("The selected file is not a scene.")
+        self.__importFileToCurrentDirectory(scene_path)
+
+    def __importFileToCurrentDirectory(self, path):
+        """
+        Import specified file to the current directory
+        """
+        if not FileSystem.IsValidFile(path):
+            raise Exception("The selected file is invalid.")
+        fileName = FileSystem.GetFileNameWithExtension(path)
+        importFilePath = FileSystem.JoinPath(self.CurrentDirectory, fileName)
+
+        if FileSystem.IsValidFile(importFilePath):
+            raise Exception(
+                "File with same name already exist in the directory")
+        FileSystem.CopyFile(path, importFilePath)
+        self.RefreshContents()
+
     def RefreshContents(self):
         """
         Updates the files and folders list when CurrentDirectory changes

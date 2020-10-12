78f87811840a77a02632ca46882f79c7cc2efa4f
Kapil Verma <sparkskapil@gmail.com>
2020-10-13 00:15:05 +0530

    Partially completed network controller script.

--- PrototypeExample/Scripts/NetworkController.py
+++ PrototypeExample/Scripts/NetworkController.py
@@ -1,7 +1,8 @@
 import socket
 import threading
 from ECS.Scriptable import Scriptable, Event, StateEvent
-from ECS.Components import ButtonComponent, TagComponent, SpriteComponent, LabelComponent
+from ECS.Components import ButtonComponent, TagComponent, LabelComponent
+from ECS.Components import Vector, TransformComponent
 import Global
 
 
@@ -10,7 +11,7 @@ class MenuButton(Scriptable):
         Scriptable.__init__(self, scene, entity)
 
     def Setup(self):
-        self.IsBusy = False
+        self.IsBusy = None
         self.Hosts = list()
         self.Lock = threading.Lock()
         self.IpCount = 0
@@ -18,22 +19,41 @@ class MenuButton(Scriptable):
             entt = self.GetEntitiesByTag("LobbyLabel_1")[0]
             label = entt.GetComponent(LabelComponent)
             label.text = "Searching hosts"
+            self.IsBusy = True
             self.FindHosts()
 
     def Update(self, timestep):
-        pass
+        if self.IsBusy == False and len(self.Hosts):
+            position = Vector(150, 265, 0)
+            for i, host in enumerate(self.Hosts):
+                position.y += i*50
+                ent = self.CreateEntity()
+                ent.GetComponent(TransformComponent).position = position
+                label = ent.AddComponent(LabelComponent(host, 'Fonts/FreeSansBold.ttf'))
+            self.IsBusy = None
+        if self.IsBusy == False and len(self.Hosts) == 0:
+            position = Vector(125, 265, 0)
+            ent = self.CreateEntity()
+            ent.GetComponent(TransformComponent).position = position
+            label = ent.AddComponent(LabelComponent())
+            label.text = 'No hosts available.'
+            label.font = 'Fonts/FreeSansBold.ttf'
+            label.size = 24
+            self.IsBusy = None
 
     def __checkIpOnThread(self, sock, address, maxCount):
+        connected = False
         if sock.connect_ex(address) == 0:
-            self.Hosts.append(address[0])
+            connected = True
         self.Lock.acquire()
         self.IpCount += 1
+        if connected:
+            self.Hosts.append(address[0])
         if self.IpCount == maxCount:
-            self.Busy = False
+            self.IsBusy = False
         self.Lock.release()
 
     def FindHosts(self):
-        self.IsBusy = True
         # getting the hostname by socket.gethostname() method
         hostname = socket.gethostname()
         # getting the IP address using socket.gethostbyname() method
